4d991925de99395e8094f70b8b37876e5171d87a
Antoine Dozois <dozois.a@gmail.com>
2017-12-10 18:34:15 -0500

    Ajout de la classe Zombie

---
+++ QSimZombie/QSimZombie/Zombie.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "QHumanoid.h"
+class Zombie : public QHumanoid
+{
+public:
+	Zombie() = delete;
+	Zombie(qreal viewRay, qreal rotationAngle, qreal walkSpeed, qreal runSpeed, QGraphicsItem *parent = nullptr);
+	~Zombie() = default;
+	void advance(int phase) override;
+	bool attack(QGraphicsItem * Human);
+
+signals:
+	void eat(QGraphicsItem * human);
+
+private:
+	static RandomIntUnif * mDeathGenerator;
+
+	static const int mBegin;
+	static const int mEnd;
+	static const int mAttackThreshold;
+};
+

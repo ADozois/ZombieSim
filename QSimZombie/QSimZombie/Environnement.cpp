cdcbbe68db21c1e2e6210c25644b0c628e25348c
Antoine Dozois <dozois.a@gmail.com>
2017-12-15 10:34:16 -0500

    Ajout de la methode de creation de ville et initialization du monde

--- QSimZombie/QSimZombie/Environnement.cpp
+++ QSimZombie/QSimZombie/Environnement.cpp
@@ -1,19 +1,26 @@
 #include "Environnement.h"
 #include <QGraphicsEllipseItem>
+#include "RandomIntUnif.h"
+#include "RandomNorm.h"
+#include "ParamSim.h"
+#include "QHumanoid.h"
+#include "Human.h"
 
 
 
 Environnement::Environnement(ParamSim *parameters)
+	:mMeanPeopleDispertion{ 0 },
+	mDevPeopleDispertion{ 10 }
 {
-	mScene = new QGraphicsScene;
-
+	mScene = new QGraphicsScene(0, 0, 800, 200);
+	mPeopleDispertion = new RandomNorm(mMeanPeopleDispertion, mDevPeopleDispertion);
+	mHeightDispertion = new RandomIntUnif(0, ParamSim::SceneHeight());
+	mWidthDispertion = new RandomIntUnif(0, ParamSim::SceneWidth());
 
 	//On cree 10 cercles juste pour dire
-	for (int i = 0; i < 10; ++i)
-	{
-		QGraphicsEllipseItem * newItem = new QGraphicsEllipseItem(-50 + i * 20, -50 + 1 * 20, 10, 10);
-		mScene->addItem(newItem);
-	}
+	
+	
+	initializeWorld();
 
 	
 
@@ -27,3 +34,38 @@ Environnement::Environnement(ParamSim *parameters)
 	//On peuple les villes en créant les humains dans chacun des groupes
 	//Certain sont enfant, certain sont militaire, certain sont contaminé
 }
+
+Environnement::~Environnement()
+{
+	delete mPeopleDispertion;
+	delete mHeightDispertion;
+	delete mWidthDispertion;
+}
+
+inline void Environnement::CreateCity(int nbrPeople)
+{
+	int x = mWidthDispertion->Generate();
+	int y = mHeightDispertion->Generate();
+	for (int i = 0; i < nbrPeople; i++)
+	{
+		QHumanoid * newItem = new Human(x + mPeopleDispertion->Generate(),y + mPeopleDispertion->Generate());
+		mScene->addItem(newItem);
+	}
+}
+
+inline int Environnement::FindPeoplePerCity(int totalHuman, int nbrCity)
+{
+	return std::floor(totalHuman/nbrCity);
+}
+
+void Environnement::initializeWorld()
+{
+	int nbrPeoplePerCity = FindPeoplePerCity(ParamSim::NbrHuman(), ParamSim::NbrCity());
+
+	for (int i = 0; i < ParamSim::NbrCity(); i++)
+	{
+		CreateCity(nbrPeoplePerCity);
+	}
+}
+
+
--- QSimZombie/QSimZombie/Environnement.h
+++ QSimZombie/QSimZombie/Environnement.h
@@ -3,17 +3,31 @@
 
 #include <QGraphicsScene>
 #include <Qlist>
+
 class ParamSim;
+class RandomNorm;
+class RandomIntUnif;
+
 
 class Environnement {
 
 
 private:
 	QGraphicsScene * mScene;
-	
+	RandomNorm * mPeopleDispertion;
+	RandomIntUnif * mHeightDispertion;
+	RandomIntUnif * mWidthDispertion;
+	RandomIntUnif * mPopulationDistribution;
+	double mMeanPeopleDispertion;
+	double mDevPeopleDispertion;
+
+	void CreateCity(int nbrPeople);
+	int FindPeoplePerCity(int totalHuman, int nbrCity);
+	void initializeWorld();
 
 public:
 	Environnement(ParamSim *parameters);
+	~Environnement();
 	QGraphicsScene *scene() { return mScene; }
 };
 

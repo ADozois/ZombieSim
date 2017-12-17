7634d9e38a3f4849df996020c8ec44b7bdd5dceb
Antoine Dozois <dozois.a@gmail.com>
2017-12-17 12:09:18 -0500

    Ajout de fonction pour le virus

--- QSimZombie/QSimZombie/Virus.h
+++ QSimZombie/QSimZombie/Virus.h
@@ -6,13 +6,19 @@ public:
 	~Virus();
 
 	int Strenght();
+	bool IsBecomingZombit();
+	void DecTimeRemaining();
+	void DecTimeRemaining(int decrement);
+	bool WillInfect(int humanStrenght);
 
 
 private:
 	int mStrenght;
+	int mTicToZombie;
 	
 	static double mMeanStrenght;
 	static double mDevStrenght;
+	static int mTimeZombie;
 
 };
 

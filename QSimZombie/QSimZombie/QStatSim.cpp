2aa063db727d7918c194660cb8c323b3e51ae251
Antoine Dozois <dozois.a@gmail.com>
2017-12-17 17:19:46 -0500

    Ajout de la classe pour les statistique

---
+++ QSimZombie/QSimZombie/QStatSim.cpp
@@ -0,0 +1,212 @@
+#include "QStatSim.h"
+#include "ParamSim.h"
+
+
+int QStatSim::mNbrZombie{ 0 };
+int QStatSim::mNbrHumain{ 0 };
+int QStatSim::mNbrHomme{ 0 };
+int QStatSim::mNbrFemme{ 0 };
+int QStatSim::mNbrEnfant{ 0 };
+int QStatSim::mNbrMilitaire{ 0 };
+int QStatSim::mNbrMort{ 0 };
+std::map<QString, int> QStatSim::mNbrNomMort{};
+std::map<QString, int> QStatSim::mNbrNomVivant{};
+std::vector<int> QStatSim::mPopEvol{};
+
+
+QStatSim::QStatSim(ParamSim * params)
+{
+	mNbrHumain = params->NbrHuman();
+}
+
+
+QStatSim::~QStatSim()
+{
+
+}
+
+void QStatSim::SetNbrZombie(int value)
+{
+	if (value > 0)
+	{
+		mNbrZombie = value;
+	}
+}
+
+void QStatSim::SetNbrHumain(int value)
+{
+	if (value > 0)
+	{
+		mNbrHumain = value;
+	}
+}
+
+void QStatSim::SetNbrHomme(int value)
+{
+	if (value > 0)
+	{
+		mNbrHomme = value;
+	}
+}
+
+void QStatSim::SetNbrFemme(int value)
+{
+	if (value > 0)
+	{
+		mNbrFemme = value;
+	}
+}
+
+void QStatSim::SetNbrEnfant(int value)
+{
+	if (value > 0)
+	{
+		mNbrEnfant = value;
+	}
+}
+
+void QStatSim::SetNbrMilitaire(int value)
+{
+	if (value > 0)
+	{
+		mNbrMilitaire = value;
+	}
+}
+
+void QStatSim::SetNbrMort(int value)
+{
+	if (value > 0)
+	{
+		mNbrMort = value;
+	}
+}
+
+void QStatSim::SetNbrNomMort(std::map<QString, int> value) {
+	mNbrNomMort = value;
+}
+void QStatSim::SetNbrNomVivant(std::map<QString, int> value) {
+	mNbrNomMort = value;
+}
+void QStatSim::SetPopEvol(std::vector<int> value) {
+	mPopEvol = value;
+};
+
+void QStatSim::IncNbrZombie()
+{
+	mNbrZombie++;
+}
+
+void QStatSim::IncNbrHumain()
+{
+	mNbrHumain++;
+}
+
+void QStatSim::IncNbrHomme()
+{
+	mNbrHomme++;
+}
+
+void QStatSim::IncNbrFemme()
+{
+	mNbrFemme++;
+}
+
+void QStatSim::IncNbrEnfant()
+{
+	mNbrEnfant++;
+}
+
+void QStatSim::IncNbrMilitaire()
+{
+	mNbrMilitaire++;
+}
+
+void QStatSim::IncNbrMort()
+{
+	mNbrMort++;
+}
+
+void QStatSim::IncNbrNomMort(QString name)
+{
+	mNbrNomMort.find(name)->second++;
+}
+
+void QStatSim::IncNbrNomVivant(QString name)
+{
+	mNbrNomVivant.find(name)->second++;
+}
+
+void QStatSim::DecNbrZombie()
+{
+	mNbrZombie--;
+}
+
+void QStatSim::DecNbrHumain()
+{
+	mNbrHumain--;
+}
+
+void QStatSim::DecNbrHomme()
+{
+	mNbrHomme--;
+}
+
+void QStatSim::DecNbrFemme()
+{
+	mNbrFemme--;
+}
+
+void QStatSim::DecNbrEnfant()
+{
+	mNbrEnfant--;
+}
+
+void QStatSim::DecNbrMilitaire()
+{
+	mNbrMilitaire--;
+}
+
+void QStatSim::DecNbrMort()
+{
+	mNbrMort--;
+}
+
+void QStatSim::DecNbrNomMort(QString name)
+{
+	mNbrNomMort.find(name)->second--;
+}
+
+void QStatSim::DecNbrNomVivant(QString name)
+{
+	mNbrNomVivant.find(name)->second--;
+}
+
+void QStatSim::UpdatePop()
+{
+	mPopEvol.push_back(mNbrHumain);
+}
+
+qreal QStatSim::PourcentageZombie(void)
+{
+	return mNbrZombie/(mNbrZombie+mNbrHumain);
+}
+
+qreal QStatSim::PourcentageHumain(void)
+{
+	return 1 - PourcentageZombie();
+}
+
+qreal QStatSim::PourcentageHomme(void)
+{
+	return 1 - PourcentageFemme() - PourcentageMilitaire();
+}
+
+qreal QStatSim::PourcentageFemme(void)
+{
+	return mNbrFemme/mNbrHumain;
+}
+
+qreal QStatSim::PourcentageMilitaire(void)
+{
+	return mNbrMilitaire/mNbrHumain;
+}
